2be98334ff69cc49561f7c0f0d27e799e3c65cab
dloyolar <dieegolr@gmail.com>
2022-06-16 21:49:38 -0400

    feat: add cors

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 class Server {
   constructor() {
@@ -12,6 +13,7 @@ class Server {
   }
 
   middlewares() {
+    this.app.use(cors());
     this.app.use(express.static('public'));
   }
 
