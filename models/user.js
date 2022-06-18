f567b70627e9e18131c62938a6ec364bff9fe365
dloyolar <dieegolr@gmail.com>
2022-06-18 14:36:16 -0400

    feat: add User model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = Schema({
+  name: { type: String, required: [true, 'name is required'] },
+  email: { type: String, required: [true, 'email is required'], unique: true },
+  password: { type: String, required: [true, 'password is required'] },
+  img: { type: String },
+  role: { type: String, required: true, enum: ['ADMIN_ROLE', 'USER_ROLE'] },
+  status: { type: Boolean, default: true },
+  google: { type: Boolean, default: false },
+});
+
+module.exports = model('User', userSchema);
