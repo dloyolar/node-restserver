ea9a210f0570f2201bcf461701da604825d8147b
dloyolar <dieegolr@gmail.com>
2022-06-25 16:56:12 -0400

    feat: add google verify function and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 
 const { generateJWT } = require('../helpers/generate-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -33,4 +34,32 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+  try {
+    const { name, img, email } = await googleVerify(id_token);
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      const data = {
+        name,
+        email,
+        password: ':D',
+        img,
+        google: true,
+      };
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.status) return res.status(401).json({ msg: 'user blocked' });
+
+    const token = await generateJWT(user.id);
+
+    res.json({ user, token });
+  } catch {
+    res.status(400).json({ ok: false, msg: 'token could not be verified' });
+  }
+};
+
+module.exports = { login, googleSignIn };
