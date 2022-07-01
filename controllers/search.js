d1ff588e6116445fb940ae4c347a28dfe09b3bd9
dloyolar <dieegolr@gmail.com>
2022-07-01 12:40:09 -0400

    feat: add initial files and functions for search

---
+++ controllers/search.js
@@ -0,0 +1,10 @@
+const { response } = require('express');
+
+const search = (req, res = response) => {
+  const { collection, term } = req.params;
+  res.json({ collection, term });
+};
+
+module.exports = {
+  search,
+};
