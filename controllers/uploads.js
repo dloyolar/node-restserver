5bc72dc336a85d793270fbc762880e750ca3aa8d
dloyolar <dieegolr@gmail.com>
2022-07-05 15:58:43 -0400

    refactor: use upload file helper in controller

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,14 @@
 const { response } = require('express');
+const { uploadFile } = require('../helpers');
 
-const loadFile = (req, res = response) => {
-  res.json({ msg: 'load file' });
+const loadFile = async (req, res = response) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
+    res.status(400).json({ msg: 'No files were uploaded.' });
+    return;
+  }
+
+  const name = await uploadFile(req.files);
+  res.json({ name });
 };
 
 module.exports = {
